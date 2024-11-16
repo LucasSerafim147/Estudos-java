ea3f4567b886f5bceb3d8021bdd937022398ecd9
Lucas Serafim <lucashenrique74148@gmail.com>
2024-11-16 15:35:20 -0300

    alo

---
+++ Estudo/src/MANOS/expressoes.java
@@ -0,0 +1,10 @@
+package MANOS;
+
+public class expressoes {
+	public static void main(String[] args) {
+		int amigos = 10;
+		//amigos = amigos + 1;
+		amigos++;
+		System.out.println(amigos);
+	}
+}
